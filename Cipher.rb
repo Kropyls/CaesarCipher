48bd07a79456a4dc71b76ccc513a0061afd47500
Kropyls <Kropyls@gmail.com>
2022-05-21 20:52:41 -0700

    took away comments since the other solutions submitted on Odin didn't have them. Can always get them back if I want

--- Cipher.rb
+++ Cipher.rb
@@ -1,28 +1,19 @@
-#constant declaration
+
 LOWERCASE = ("a".."z").to_a
 UPPERCASE = ("A".."Z").to_a
 
 def caesar_cipher(string, shift)
-  #set up the roted arrays in both upper and lowercase
-  #would be nice if there was a method to check if a char was caps or lowercase
   rotated_lowercase = LOWERCASE.rotate(shift)
   rotated_uppercase = UPPERCASE.rotate(shift)
-  #break the input string into individual chars
-  chars = string.chars
-  #now the fun starts: there are 2 includes? checks to preserve the casing of the char
-  cipher = chars.map do |c|
-    #check if the char is a letter
+  
+  cipher = string.chars.map do |c|
     if LOWERCASE.include?(c)
-      #and return the rotated index of the letter if true
       rotated_lowercase[LOWERCASE.find_index(c)]
-    #duplicate of the above lines but for upper case
     elsif UPPERCASE.include?(c)
       rotated_uppercase[UPPERCASE.find_index(c)]
     else
-      #don't change the char if it isn't a letter
       c
     end
   end
-  #and put it back to a string
   cipher.join()
 end
