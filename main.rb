232cebe9313304e8c1d3afdf8d3f07af36b033b0
Kropyls <Kropyls@gmail.com>
2022-09-16 20:49:13 -0700

    removed positive number requirement from main prompt.

--- main.rb
+++ main.rb
@@ -8,7 +8,7 @@ puts 'Input message to encode now, press enter, then enter the shift value.'
 puts 'Enter "done" when finished.'
 
 input = gets.chomp
-puts 'Input shift value now. Value must be a positive integer.'
+puts 'Input shift value now. Value must be an integer.'
 shift = cipher.get_shift(gets.chomp)
 
 until input == 'done' || shift == 'done'
