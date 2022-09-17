bd9451d94d357a24e7e7e904d6ff9b779c0aa2a2
Kropyls <Kropyls@gmail.com>
2022-09-16 20:10:25 -0700

    rebuilt cipher into object, and added main

---
+++ main.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require './lib/cipher'
+
+cipher = Cipher.new
+
+puts 'Input message to encode now, press enter, then enter the shift value.'
+puts 'Enter "done" when finished.'
+
+input = gets.chomp
+puts 'Input shift value now. Value must be a positive integer.'
+shift = cipher.get_shift(gets.chomp)
+
+until input == 'done' || shift == 'done'
+  puts cipher.caesar_cipher(input, shift)
+
+  input = gets.chomp
+  break if input == 'done'
+
+  shift = cipher.get_shift(gets.chomp)
+end
+
+puts 'finished.'
